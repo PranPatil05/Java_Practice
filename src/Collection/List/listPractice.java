810d5ff90d34f0ecd9a3b4aea8eff41dec082e42
PranPatil05 <pranav.patil050996@gmail.com>
2022-12-04 21:47:52 +0530

    List Practice

--- src/Collection/List/listPractice.java
+++ src/Collection/List/listPractice.java
@@ -1,6 +1,29 @@
 package Collection.List;
 
-public class listPractice {
+import java.util.ArrayList;
+import java.util.List;
 
+public class listPractice {
+    public static void main(String[] args) {
+        ArrayList al=new ArrayList<>();
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add('A');
+        al.add('B');
+        al.add('C');
+        al.add('A');
+        al.add("Pranav");
+        al.add("Patil");
+
+        System.out.println(al);
+
+        System.out.println(al.get(1)); //2
+        System.out.println(al.contains("Pranav")); //true
+        System.out.println(al.contains("pranav")); //false
+        System.out.println(al.isEmpty()); //false
+
+
+    }
 
 }
