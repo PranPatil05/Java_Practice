fc10b3661cb287206d394e2f8e49ef5bdf25e1c3
PranPatil05 <pranav.patil050996@gmail.com>
2022-12-16 15:20:18 +0530

    HashSet Practice

---
+++ src/Collection/Set/HashsetDemo.java
@@ -0,0 +1,36 @@
+package Collection.Set;
+
+import Collection.Student;
+
+import java.util.HashSet;
+
+public class HashsetDemo {
+    public static void main(String[] args) {
+        HashSet hs=new HashSet();
+
+        hs.add(1);
+        hs.add(2);
+        hs.add(3);
+        hs.add('A');
+        hs.add('B');
+        hs.add('C');
+        hs.add(1);
+        hs.add('B');
+
+        System.out.println(hs);
+
+        Student1 s1=new Student1(10,"Ram");
+        Student1 s2=new Student1(20,"Ram");
+        Student1 s3=new Student1(300,"Ram");
+        Student1 s4=new Student1(300,"Ram");
+
+        HashSet hs1=new HashSet();
+
+        hs1.add(s1);
+        hs1.add(s2);
+        hs1.add(s3);
+
+        System.out.println(hs1);
+
+    }
+}
